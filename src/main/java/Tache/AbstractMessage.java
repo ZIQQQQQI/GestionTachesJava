025ff8e079dd0336271ba324613ca35991017560
silu <0827silu@gmail.com>
2020-10-19 22:41:53 +0200

    good

--- src/main/java/Tache/AbstractMessage.java
+++ src/main/java/Tache/AbstractMessage.java
@@ -1,14 +1,18 @@
 package Tache;
 
-public class AbstractMessage {
+import Compte.AbstractIntervenant;
+
+public abstract class AbstractMessage {
 
         protected String date;
         protected String contenu;
-        protected String expediteur;
+
 
         public AbstractMessage(String date, String contenu) {
             this.date = date;
             this.contenu = contenu;
         }
+
+
 
 }
