240f0b8c40cb3301546b0f449422b30b8c610c79
Afaf-24 <afaf.ar@outlook.fr>
2020-10-19 16:00:14 +0200

    test

---
+++ src/main/java/Compte/Intervenant.java
@@ -0,0 +1,18 @@
+package Compte;
+
+import java.util.ArrayList;
+
+public class Intervenant extends AbstractIntervenant {
+    private ArrayList<>
+    public Intervenant() {
+
+    }
+
+    public void envoyerMessageReclamation(Message msg, AbstractReclamation r){
+
+    }
+
+    public void accepterTache(){
+
+    }
+}
