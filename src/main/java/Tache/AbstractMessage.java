f8d0c6ee1279e112b0b9c73401f81185855ff55f
TANG <tangziqi1226@gmail.com>
2020-10-19 16:43:28 +0200

    v1

--- src/main/java/Tache/AbstractMessage.java
+++ src/main/java/Tache/AbstractMessage.java
@@ -4,7 +4,7 @@ public class AbstractMessage {
 
         protected String date;
         protected String contenu;
-        protected String expediteur;
+
 
         public AbstractMessage(String date, String contenu) {
             this.date = date;
